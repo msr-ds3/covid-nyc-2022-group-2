ad8bb95eb81368f8c395f2a8629bde053b38319a
Daniel Glick <glickdaniel98@gmail.com>
2022-06-24 16:20:03 +0000

    cleaning

--- 02_download_covid_counts_april1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# use curl or wget to download the version 2 of the total counts file, googlebooks-eng-all-totalcounts-20120701.txt
-curl https://raw.githubusercontent.com/nychealth/coronavirus-data/097cbd70aa00eb635b17b177bc4546b2fce21895/tests-by-zcta.csv > tests-by-zcta_april1st.csv
-# update the timestamp on the resulting file using touch
-# do not remove, this will keep make happy and avoid re-downloading of the data once you have it
-touch tests-by-zcta_april1st.csv
--- 03_download_covid_counts_may1st.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# use curl or wget to download the version 2 of the total counts file, googlebooks-eng-all-totalcounts-20120701.txt
-curl https://raw.githubusercontent.com/nychealth/coronavirus-data/9e26adc2c475d3378d7579e48e936f8a807b254b/tests-by-zcta.csv > tests-by-zcta_may1st.csv
-# update the timestamp on the resulting file using touch
-# do not remove, this will keep make happy and avoid re-downloading of the data once you have it
-touch tests-by-zcta_may1st.csv
